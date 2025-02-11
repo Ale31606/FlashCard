e45d635133cd15e4c73f78573272ee442f994e10
Ale31606 <143433328+Ale31606@users.noreply.github.com>
2025-02-10 21:11:26 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+/* 
+
+Para que de vuelta dando click
+
+document.querySelectorAll('.card').forEach(card => {
+    card.addEventListener('click', () => {
+        card.classList.toggle('flipped');
+    });
+});*/
